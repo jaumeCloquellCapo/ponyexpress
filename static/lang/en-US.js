49060804ab75b0451655576ea8617114cc34bd49
Jaume cloquell capo <jaumecloquell@hotmail.com>
2021-05-13 16:22:39 +0200

    .

--- static/lang/en-US.js
+++ static/lang/en-US.js
@@ -21,24 +21,24 @@ export default () => {
         title: 'Gallery'
       },
       home: {
-        our_services: 'Our servicies',
-        party: 'FIESTAS DE CUMPLEAÑOS',
-        party_desc: 'Celebra el cumpleaños de los peques , de una forma diferente, al aire libre y en contacto con los animales',
-        comunion:  'PRIMERA COMUNIÓN',
-        comunion_desc: 'La mejor opción para la celebración de una comunión es con Pony Express. Donde los niños son los verdaderos protagonistas. Nuestros cariñosos ponys harán que el día se convierta en inolvidable para todos.',
-        others: 'OTRAS CELEBRACIONES',
-        others_desc: ' Te ofrecemos la posibilidad de organizar los mejores eventos, fiestas y celebraciones. Ofrecemos servicios personalizados.'
+        our_services: 'Our services',
+        party: 'BIRTHDAY PARTIES',
+        party_desc: 'Celebrate the birthday of the kids, in a different way, outdoors and in contact with animals', 
+        comunion: 'FIRST COMMUNION',
+        comunion_desc: 'The best option for the celebration of a communion is with Pony Express. Where the children are the real protagonists. Our affectionate ponies will make the day unforgettable for everyone',
+        others: 'OTHER CELEBRATIONS',
+        others_desc: ' We offer you the possibility to organise the best events, parties and celebrations. We offer personalised services.', 
       },
       contact: {
         title: 'Contact',
-        subtitle:'¿ Odias los formularios ? Llámanos directamente en su lugar'
+        subtitle:'Do you hate forms? Call us directly instead'
       },
       hero: {
         title: 'Horses are the favourite animal of many children and on this pony ride in Mallorca they can see their dream come true.'
       },
       call: {
-        visit: '¡ Ven a visitarnos sin compromiso !',
-        visit_desc: 'Podrás ver y elegir entre los ponis que forman parte de nuestro equipo, un lujo al alcance de la mano.'
+        visit: 'Come and visit us with no obligation',
+        visit_desc: 'You will be able to see and choose between the ponies that are part of our team, a luxury within reach',
       }
     })
   })
