2ba105a4f2e42cfaa802de80355b2edc41e50da9
Jaume cloquell capo <jaumecloquell@hotmail.com>
2021-05-13 13:26:52 +0200

    .

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,77 +1,113 @@
 <template>
-  <header class="toolbar">
-    <div class="toolbar--container">
-      <NuxtLink :to="localePath('index')"  class="text--logo text-gray-900 from-blue-600 via-indigo-400 to-purple-300">
+  <nav :class="headerClassList" class="w-full z-30 top-0">
+    <div
+      class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2"
+    >
+      <div class="pl-4 flex items-center">
+         <NuxtLink :to="localePath('index')"  class="text--logo text-gray-600">
         Pony Express Mallorca
       </NuxtLink>
+      </div>
+      <div class="block lg:hidden pr-4">
+        <button
+          class="flex items-center p-1 text-orange-800 hover:text-gray-900"
+          @click.prevent.stop="onToggleClick"
+        >
+          <svg
+            class="fill-current h-6 w-6"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+      </div>
 
-      <div class="relative z-10 inline-flex items-center space-x-3 md:ml-5 lg:justify-end">
-        <nav class="hidden md:block">
-          <div class="flex items-baseline space-x-10">
-            <NuxtLink :to="localePath('index')"  class="item--link">
+      <div
+        :class="navContentClassList"
+        class="w-full flex-grow lg:flex lg:items-center lg:w-auto lg:block mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20"
+      >
+        <ul class="list-reset lg:flex justify-end flex-1 items-center">
+          <li class="mr-3">
+             <NuxtLink :to="localePath('index')"  class="item--link">
               <span class="block">Home</span>
             </NuxtLink>
+          </li>
 
-                <!--NuxtLink :to="localePath('gallery')"  class="item--link">
-              <span class="block">     {{ $t('gallery.title') }}</span>
-            </NuxtLink-->
-
-
-                 <NuxtLink
+          <li class="mr-3">
+            <NuxtLink
               v-for="locale in availableLocales"
-              class="block"
+              class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
               :key="locale.code"
               :to="switchLocalePath(locale.code)"
             >
               {{ $t('common.' + locale.code) }}
             </NuxtLink>
-
-            <span class="inline-flex rounded-md shadow-sm">
+          </li>
+        </ul>
+      <span class="inline-flex rounded-md shadow-sm">
                   <NuxtLink :to="localePath('contact')" class="btn btn--primary">
                           {{ $t('contact.title') }} 
                   </NuxtLink>
             </span>
-          </div>
-        </nav>
-
       </div>
     </div>
-  </header>
+    <hr class="border-b border-gray-100 opacity-25 my-0 py-0" />
+  </nav>
 </template>
 
 <script>
+
+
 export default {
-  name: 'Navbar',
-   computed: {
+  name: 'TheHeader',
+  data() {
+    return {
+      scrollY: 0,
+      isOpen: false,
+    }
+  },
+  computed: {
     availableLocales() {
       return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale)
     },
+    isSticky() {
+      return this.scrollY > 15
+    },
+    headerClassList() {
+      return this.isSticky ? 'bg-white shadow' : ''
+    },
+    navActionClassList() {
+      return this.isSticky ? 'text-black' : 'bg-white text-gray-800'
+    },
+    navContentClassList() {
+      let classList = this.isSticky ? 'bg-white' : 'bg-gray-100'
+      if (!this.isOpen) {
+        classList += ` hidden`
+      }
+      return classList
+    },
+  },
+  methods: {
+    onClick() {
+      this.isOpen = false
+    },
+    onScroll() {
+      this.scrollY = window.scrollY
+    },
+    onToggleClick() {
+      this.isOpen = !this.isOpen
+    },
+  },
+  mounted() {
+    this.scrollY = window.scrollY
+    document.addEventListener('click', this.onClick)
+    document.addEventListener('scroll', this.onScroll)
+  },
+  beforeDestroy() {
+    document.removeEventListener('click', this.onClick, true)
+    document.removeEventListener('scroll', this.onScroll, true)
   },
 }
 </script>
-
-<style lang="postcss" scoped>
-.toolbar {
-  @apply relative w-full px-8 text-gray-700 bg-white;
-}
-
-.toolbar .toolbar--container {
-  @apply container flex flex-col flex-wrap items-center justify-between py-5 mx-auto max-w-7xl;
-
-  @screen md {
-    @apply flex-row;
-  }
-}
-
-.toolbar .text--logo {
-  @apply relative z-10 flex items-center w-auto text-2xl font-extrabold leading-none select-none;
-}
-
-nav .item--link {
-  @apply relative font-medium leading-6 mx-2 text-gray-600 transition duration-150 ease-out;
-}
-
-nav .item--link:hover {
-  @apply text-gray-900;
-}
-</style>
---
+++ components/Navbar2.vue
@@ -0,0 +1,77 @@
+<template>
+  <header class="toolbar">
+    <div class="toolbar--container">
+      <NuxtLink :to="localePath('index')"  class="text--logo text-gradient bg-gradient-to-r from-blue-600 via-indigo-400 to-purple-300">
+        Pony Express Mallorca
+      </NuxtLink>
+
+      <div class="relative z-10 inline-flex items-center space-x-3 md:ml-5 lg:justify-end">
+        <nav class="hidden md:block">
+          <div class="flex items-baseline space-x-10">
+            <NuxtLink :to="localePath('index')"  class="item--link">
+              <span class="block">Home</span>
+            </NuxtLink>
+
+                <NuxtLink :to="localePath('gallery')"  class="item--link">
+              <span class="block">     {{ $t('gallery.title') }}</span>
+            </NuxtLink>
+
+
+                 <NuxtLink
+              v-for="locale in availableLocales"
+              class="block"
+              :key="locale.code"
+              :to="switchLocalePath(locale.code)"
+            >
+              {{ $t('common.' + locale.code) }}
+            </NuxtLink>
+
+            <span class="inline-flex rounded-md shadow-sm">
+                  <NuxtLink :to="localePath('contact')" class="btn btn--primary">
+                          {{ $t('contact.title') }} 
+                  </NuxtLink>
+            </span>
+          </div>
+        </nav>
+
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+   computed: {
+    availableLocales() {
+      return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale)
+    },
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.toolbar {
+  @apply relative w-full px-8 text-gray-700 bg-white;
+}
+
+.toolbar .toolbar--container {
+  @apply container flex flex-col flex-wrap items-center justify-between py-5 mx-auto max-w-7xl;
+
+  @screen md {
+    @apply flex-row;
+  }
+}
+
+.toolbar .text--logo {
+  @apply relative z-10 flex items-center w-auto text-2xl font-extrabold leading-none select-none;
+}
+
+nav .item--link {
+  @apply relative font-medium leading-6 mx-2 text-gray-600 transition duration-150 ease-out;
+}
+
+nav .item--link:hover {
+  @apply text-gray-900;
+}
+</style>
