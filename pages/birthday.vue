dbb22f4899c7ebfe15192834d9dc24b087eee7c6
Jaume Cloquell Capo <jaumecloquell@hotmail.com>
2021-09-27 18:55:00 +0000

    .

--- pages/birthday.vue
+++ pages/birthday.vue
@@ -6,7 +6,7 @@
           <h1 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
            {{ $t('birthday.title') }}
             <br>
-            
+
             <span class="text-indigo-600">Pony express</span>
           </h1>
           <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
@@ -24,19 +24,19 @@
           <dt>
             <h2 class="text-lg leading-6 font-medium text-gray-900">  {{ $t('birthday.service_home_house') }} </h2>
           </dt>
-          <dd class="mt-2 text-base text-gray-500">
+          <h3 class="mt-2 text-base text-gray-500">
             {{ $t('birthday.service_home_house_desc') }}
 
-          </dd>
+          </h3>
         </div>
 
         <div class="relative">
           <dt>
             <h2 class="text-lg leading-6 font-medium text-gray-900"> {{ $t('birthday.service_house') }} </h2>
           </dt>
-          <dd class="mt-2 text-base text-gray-500">
+          <h3 class="mt-2 text-base text-gray-500">
                 {{ $t('birthday.service_house_desc') }}
-          </dd>
+          </h3>
         </div>
       </dl>
     </div>
