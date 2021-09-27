e31ce503ec7cb589ea34d34b4e987a2cbedf9029
Jaume Cloquell Capo <jaumecloquell@hotmail.com>
2021-09-27 18:32:24 +0000

    .

--- pages/birthday.vue
+++ pages/birthday.vue
@@ -1,25 +1,26 @@
 <template>
   <section class="hero md:px-0">
-    <div class="hero--container">
-      <h1 class="hero--headings">
-        <span class="block">Fiesta de cumpleaños con <span class="block text-gradient bg-gradient-to-r from-blue-600 via-indigo-400 to-purple-300 lg:inline lg:mt-0 mt-1">Pony express</span></span>
-      </h1>
-      <p class="hero--description">
-         Lo importante es celebrar y pasar un buen momento!!!
-      </p>
-      <!--div class="hero--actions md:space-x-4">
-        <a href="#" class="btn btn--primary">
-          A domicilio
-        </a>
-        <a href="#" class="btn btn--default">
-          En nuestras instalaciones
-        </a>
-      </div-->
+     <div class="flex items-center justify-center">
+    <main class="max-w-screen-xl px-4 lg:px-16">
+        <div class="text-center sm:text-left">
+          <h1 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+           Fiesta de cumpleaños con
+            <br>
+            <span class="text-indigo-600">Pony express</span>
+          </h1>
+          <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+               Lo importante es celebrar y pasar un buen momento!!!
+          </p>
+        </div>
+      </main>
+  </div>
+
+    <div class="sm:items-center sm:justify-center hero--container">
     <div class="mt-10">
       <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
         <div class="relative">
           <dt>
-            <h2 class="text-lg leading-6 font-medium text-gray-900">   A domicilio</h2>
+            <h2 class="text-lg leading-6 font-medium text-gray-900">Servicio a domicilio</h2>
           </dt>
           <dd class="mt-2 text-base text-gray-500">
             Nada más cómodo que montar tu fiesta de cumpleaños en casa, en el local comunitario, en un restaurante de tu barrio donde tengas confianza, en un local infantil del barrio, en una casa rural en el campo, en una piscina o en el jardín de tu chalet.
