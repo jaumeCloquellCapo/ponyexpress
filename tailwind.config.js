9ac70d8b88ce41d8d21f12aa46e56e951db968dc
Jaume Cloquell Capo <jaumecloquell@hotmail.com>
2021-09-27 17:46:23 +0000

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: {
+    content: [
+      `components/**/*.{vue,js}`,
+      `layouts/**/*.vue`,
+      `pages/**/*.vue`,
+      `plugins/**/*.{js,ts}`,
+      `nuxt.config.{js,ts}`
+    ]
+  },
   theme: {
     extend: {
       colors: {
